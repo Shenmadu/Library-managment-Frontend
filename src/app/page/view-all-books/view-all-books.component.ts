e04b51c107ae06a2b83a949e7226024cf828887d
shenmadu <tashen.madu@gmail.com>
2024-02-01 22:53:46 +0530

    delete method in table

--- src/app/page/view-all-books/view-all-books.component.ts
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -1,34 +1,53 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';//inheritance 
-import { FormsModule } from '@angular/forms';import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule],//without app module ts import http clientModule
+  imports: [HttpClientModule, FormsModule, CommonModule],//without app module ts import http clientModule
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
-export class ViewAllBooksComponent implements OnInit{
+export class ViewAllBooksComponent implements OnInit {
   private http;
-  public booklist:any={};
+  public booklist: any = {};
+  public selectedBook: any;
+  
 
-  constructor(private httpClient : HttpClient){
-    this.http=httpClient;
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
   }
   ngOnInit(): void {
     this.loardBooks();
   }
 
-  loardBooks(){
+  loardBooks() {
     this.http.get("http://localhost:8080/book/get") //asyncronize process
-    .subscribe((data)=>{
-      this.booklist=data;
-      console.log(data);
-      
-    })
+      .subscribe((data) => {
+        this.booklist = data;
+        console.log(data);
+
+      })
   }
 
+  deleteBook() {
+    let api = "http://localhost:8080/book/" + this.selectedBook.id;
+    this.http.delete(api,{responseType:'text'}).subscribe((responce:string) => {
+      console.log("data");
+      
+      this.loardBooks();
+      this.selectedBook = null;
+
+
+    })
+
+  }
+  setSelectedBook(book: any) {
+    this.selectedBook = book;
+    console.log(book.id);
+
+  }
 }
