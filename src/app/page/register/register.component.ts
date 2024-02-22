a24d155c9a72ed156962ff1afefc9826d7ee6b72
shenmadu <tashen.madu@gmail.com>
2024-02-22 12:44:05 +0530

    register burroer form

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,40 +1,74 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   private http;
-  public countryList:any;
-  public selectedCountry:any;
+  public countryList: any;
+  public selectedCountry: any;
 
-  constructor(private httpClient: HttpClient){
-    this.http=httpClient;
+
+
+
+  public burrower = {
+    burrowerFirstName: null,
+    burrowerLastName: null,
+    address1: null,
+    address2: null,
+    contactNumber: null,
+    email: null,
+    country: null,
+  }
+
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
   }
 
   ngOnInit(): void {
     this.loardCountries();
-    
+
   }
-  loardCountries(){
-    let api="https://restcountries.com/v3.1/all";
-    this.http.get(api).subscribe(res =>{
-      this.countryList=res;
+  loardCountries() {
+    let api = "https://restcountries.com/v3.1/all";
+    this.http.get(api).subscribe(res => {
+      this.countryList = res;
       console.log(res);
-      
+
     });
   }
-  setSelectedCountry(country:any){
-    this.selectedCountry=country;
+  setSelectedCountry(country: any) {
+    this.selectedCountry = country;
     console.log(this.setSelectedCountry);
-    
+
+  }
+  saveBurrower() {
+    this.http.post("http://localhost:8081/burrow/add", this.burrower)
+      .subscribe((data) => {        
+        this.burrower = {
+          burrowerFirstName: null,
+          burrowerLastName: null,
+          address1: null,
+          address2: null,
+          contactNumber: null,
+          email: null,
+          country: null,
+        }
+        Swal.fire({
+          title: "Register Sucess!",
+          text: `Burrower Register succesfull`,
+          icon: "success"
+        });
+
+      })
   }
 
 }
