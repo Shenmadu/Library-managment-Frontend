3c3f1176c609a98c4e122a9092ec0a5322e190a3
shenmadu <tashen.madu@gmail.com>
2024-03-11 23:42:07 +0530

    sweet alert add login and log out

--- src/app/common/nav/nav.component.ts
+++ src/app/common/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -10,5 +11,34 @@ import { RouterLink } from '@angular/router';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  constructor(private router: Router) { }
+
+  logOut() {
+    let timerInterval: any;
+    let timer: any = "";
+    Swal.fire({
+      title: "Auto close alert!",
+      html: "I will close in <b></b> milliseconds.",
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        timer = Swal.getPopup()?.querySelector("b");
+        timerInterval = setInterval(() => {
+          timer.textContent = `${Swal.getTimerLeft()}`;
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      }
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        console.log("I was closed by the timer");
+        this.router.navigate(["/login"])
+      }
+    });
+    
+  }
 
 }
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -3,33 +3,49 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,HttpClientModule,CommonModule,FormsModule],
+  imports: [RouterLink, HttpClientModule, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  
-  public user:any={
-    email:"",
-    password:""
-  }
-  constructor(private http:HttpClient ,private router:Router){};
 
-  logIn(){
-    this.http.post("http://localhost:8081/login/request-login",this.user)
-    .subscribe((res:any)=>{
-      console.log(res);
-      if(res==true){        
-        this.router.navigate(['/home']);
-        
-      }else{
-        console.log("invalid");
-      }
-      
-    })
+  public user: any = {
+    email: "",
+    password: ""
+  }
+  constructor(private http: HttpClient, private router: Router) { };
+
+  logIn() {
+    this.http.post("http://localhost:8081/login/request-login", this.user)
+      .subscribe((res: any) => {
+        console.log(res);
+        if (res == true) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Welcome!",
+            showConfirmButton: false,
+            timer: 1500
+          });
+          this.router.navigate(['/home']);
+
+        } else {
+          Swal.fire({
+            icon: "error",
+            position: "top-end",
+            title: "Oops...",
+            text: "Something went wrong try agin!",
+            showConfirmButton: false,
+            timer: 1500
+          });
+          console.log("invalid");
+        }
+
+      })
   }
 }
