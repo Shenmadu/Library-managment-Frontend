eea9c6bdbd06ed838084c3fa1a0eb78f0c5ddea0
shenmadu <tashen.madu@gmail.com>
2024-03-12 12:04:13 +0530

    burrow and book table resposive set

--- src/app/page/add-books/add-books.component.ts
+++ src/app/page/add-books/add-books.component.ts
@@ -3,38 +3,46 @@ import { NavComponent } from '../../common/nav/nav.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-books',
   standalone: true,
-  imports: [NavComponent,HttpClientModule,FormsModule,CommonModule],
+  imports: [NavComponent, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './add-books.component.html',
   styleUrl: './add-books.component.css'
 })
 export class AddBooksComponent {
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  public book={
-    isbn:"",
-    title:"",
-    author:"",
-    qty:"",
-    catagory:""
+  public book = {
+    isbn: "",
+    title: "",
+    author: "",
+    qty: "",
+    catagory: ""
   }
-  addBook(){
-    this.http.post("http://localhost:8080/book/add",this.book)
-    .subscribe((data)=>{  
-      this.book={
-        isbn:"",
-        title:"",
-        author:"",
-        qty:"",
-        catagory:""
-      }      
-      
-      
-    })
-    
-    
+  addBook() {
+    this.http.post("http://localhost:8080/book/add", this.book)
+      .subscribe((data) => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: `${this.book.title} book added succes full`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+        this.book = {
+          isbn: "",
+          title: "",
+          author: "",
+          qty: "",
+          catagory: ""
+        }
+
+
+      })
+
+
   }
 }
--- src/app/page/burrow-book/burrow-book.component.ts
+++ src/app/page/burrow-book/burrow-book.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../common/nav/nav.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-burrow-book',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,HttpClientModule,CommonModule,FormsModule],
   templateUrl: './burrow-book.component.html',
   styleUrl: './burrow-book.component.css'
 })
 export class BurrowBookComponent {
+  constructor(private http:HttpClient){}
 
+  public userName:any;
+
+  serach(){
+    this.http.get(`http://localhost:8081/burrow/getburrower/${this.userName}`)
+    .subscribe((data)=>{
+      console.log(data);
+    })
+    
+    
+  }
 }
